f3343575540814dfc5b0b83b2d0a3cf34b0ce874
StivenSalcedo <stiven.salcedo@gmail.com>
2025-10-08 18:49:22 -0500

    arreglar seeder

--- database/seeders/FactusConfigurationSeeder.php
+++ database/seeders/FactusConfigurationSeeder.php
@@ -10,18 +10,18 @@ class FactusConfigurationSeeder extends Seeder
 {
     public function run()
     {
-        $url = App::environment('production') ? 'https://api.factus.com.co/' : 'http://api.test/';
+        $url = App::environment('production') ? 'https://api.factus.com.co/' : 'https://api-sandbox.factus.com.co/';
 
         $configurations = [
             'url' => $url,
-            'client_id' => '',
-            'client_secret' => '',
-            'email' => '',
-            'password' => ''
+            'client_id' => '9fce79f0-5b7e-4415-b59c-509599d0df1d',
+            'client_secret' => 'QP1LCJjVIUEiipIh4uewQoQJ2lEu0XEQ2OsIUhJc',
+            'email' => 'sandbox@factus.com.co',
+            'password' => 'sandbox2024%'
         ];
 
         FactusConfiguration::create([
-            'is_api_enabled' => false,
+            'is_api_enabled' => true,
             'api' => $configurations
         ]);
     }
